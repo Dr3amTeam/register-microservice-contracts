53affe80f26a89fffbf7217bfcaa7cb796ed5d3f
DanielJaureguiGonzales <dank_lp123@outlook.com>
2021-09-29 16:43:25 -0500

    Commands and events

---
+++ src/main/java/com/dhome/registermicroservice/contracts/commands/EditUser.java
@@ -0,0 +1,18 @@
+package com.dhome.registermicroservice.contracts.commands;
+
+import lombok.Value;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Value
+public class EditUser {
+    @TargetAggregateIdentifier
+    private Long Id;
+    private String Name;
+    private String LastName;
+    private Long Age;
+    private Long DNI;
+    private String Password;
+    private String Username;
+    private String Address;
+
+}
---
+++ src/main/java/com/dhome/registermicroservice/contracts/events/UserRegistered.java
@@ -0,0 +1,18 @@
+package com.dhome.registermicroservice.contracts.events;
+
+import lombok.Value;
+
+import java.time.Instant;
+
+@Value
+public class UserRegistered {
+    private Long Id;
+    private String Name;
+    private String LastName;
+    private Long Age;
+    private Long DNI;
+    private String Password;
+    private String Username;
+    private String Address;
+    private Instant occurredOn;
+}
