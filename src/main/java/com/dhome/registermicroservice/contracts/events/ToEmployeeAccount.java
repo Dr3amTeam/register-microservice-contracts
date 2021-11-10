ab5fb849b4260d62a83a9245342de31cd48736b3
DanielJaureguiGonzales <dank_lp123@outlook.com>
2021-11-09 20:37:07 -0500

    Payment1

--- src/main/java/com/dhome/registermicroservice/contracts/events/ToEmployeeAccount.java
+++ src/main/java/com/dhome/registermicroservice/contracts/events/ToEmployeeAccount.java
@@ -1,8 +1,11 @@
 package com.dhome.registermicroservice.contracts.events;
 
+import lombok.Value;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
+@Value
 public class ToEmployeeAccount {
     private String employeeId;
     private String transactionId;
